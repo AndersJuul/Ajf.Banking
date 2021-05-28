dc9abab512c02eb52b4632eb8a2ebb269e8729f0
Anders Juul <andersjuulsfirma@gmail.com>
2021-05-28 12:10:51 +0200

    .

--- Ajf.Banking/Console/Program.cs
+++ Ajf.Banking/Console/Program.cs
@@ -4,62 +4,78 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using EventSourcingTaskApp;
+using EventSourcingTaskApp.Core;
+using EventSourcingTaskApp.Infrastructure;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Task = System.Threading.Tasks.Task;
 
 namespace Consolex
 {
-    public class TestEvent
-    {
-        public string EntityId { get; set; }
-        public string ImportantData { get; set; }
-    }
     class Program
     {
         private static IEventStoreConnection _connection;
+        private static AggregateRepository _aggregateRepository;
 
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-			CancellationTokenSource tokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = tokenSource.Token;
-
-            _connection = EventStoreConnection.Create(
-                new IPEndPoint(
-                    IPAddress.Parse("127.0.0.1"),
-                    1113
-                )
-            );
-            await _connection.ConnectAsync();
+            var serviceCollection = new ServiceCollection();
+            var configurationRoot = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            new Startup(configurationRoot).ConfigureServices(serviceCollection);
             
+            _aggregateRepository = serviceCollection.BuildServiceProvider().GetRequiredService<AggregateRepository>();
+
+            var newGuid = Guid.NewGuid();
+            await Create(newGuid, "name of");
+
+            await Assign(newGuid, "Anders");
+
+            await Move(newGuid, BoardSections.InProgress);
+            await Move(newGuid, BoardSections.Done);
+
+            await Complete(newGuid);
+        }
+
+        public static async Task Create(Guid id, string title)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync<EventSourcingTaskApp.Core.Task>(id);
+            aggregate.Create(id, title, "Ahmet KÜÇÜKOĞLU");
+
+            await _aggregateRepository.SaveAsync(aggregate);
+        }
+
+
+        public static async Task Assign(Guid id, string assignedTo)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync<EventSourcingTaskApp.Core.Task>(id);
+            aggregate.Assign(assignedTo, "Ahmet KÜÇÜKOĞLU");
+
+            await _aggregateRepository.SaveAsync(aggregate);
+        }
+
+        public static async Task Move(Guid id, BoardSections section)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync< EventSourcingTaskApp.Core.Task >(id);
+            aggregate.Move(section, "Ahmet KÜÇÜKOĞLU");
+
+            await _aggregateRepository.SaveAsync(aggregate);
+        }
+
+        public static async Task Complete(Guid id)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync<EventSourcingTaskApp.Core.Task>(id);
+            aggregate.Complete("Ahmet KÜÇÜKOĞLU");
+
+            await _aggregateRepository.SaveAsync(aggregate);
             
-            var evt = new TestEvent
-            {
-                EntityId = Guid.NewGuid().ToString("N"),
-                ImportantData = "I wrote my first event!"
-            };
-
-
-
-            await _connection.AppendToStreamAsync(
-                "stream-name",
-                ExpectedVersion.Any,
-                new EventData(
-                    Guid.NewGuid(),
-                    evt.GetType().FullName,
-                    false,
-                    Encoding.UTF8.GetBytes(evt.ToString()),
-                    new byte[] { }
-                )
-            );
-
-
-            var result = await _connection.ReadStreamEventsForwardAsync(
-                "some-stream",
-                StreamPosition.Start,1 ,true);
-
-            var events = result.Events;
         }
 	}
 }
